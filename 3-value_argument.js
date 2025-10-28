f8dbbce9e13c955509bbeda5b240e1fd93d2356d
celine <celine.himbaza@gmail.com>
2025-10-28 11:13:28 +0200

    argument change

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
-const arg= process.argv.slice(2);
+const arg = process.argv.slice(2);
 
-
-if(arg[0]===undefined){
-    console.log('no argumet fund');
-}
-else{
+if (arg[0] === undefined) {
+    console.log('No argument');
+} else if (arg[1] === undefined) {
     console.log(arg[0]);
+} else {
+    console.log(arg.join(' '));
 }
